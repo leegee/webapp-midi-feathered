e602b226b873c43dba92d18115de87ba0b999c32
Lee Goddard <leegee@gmail.com>
2024-02-20 13:32:50 +0100

    Docs

--- src/components/MIDI.jsx
+++ src/components/MIDI.jsx
@@ -1,3 +1,6 @@
+/** https://developer.mozilla.org/en-US/docs/Web/API/Web_MIDI_API  */
+
+
 import { useEffect } from 'react';
 import { useAtom } from 'jotai';
 import { midiAccessAtom } from '../lib/midi';
