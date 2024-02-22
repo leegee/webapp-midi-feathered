208465f3c2c94c24f222f270a12798bf29a9200a
Lee Goddard <leegee@gmail.com>
2024-02-22 11:53:26 +0100

    Config tests

--- __tests__/midi-message.test.js
+++ __tests__/midi-message.test.js
@@ -1,9 +1,12 @@
+import '@testing-library/jest-dom'; 
 import { render, screen } from '@testing-library/react';
-import { test, expect } from '@jest/globals';
-import MyComponent from '../src/components/ScaleSelector';
 
-test('renders component with correct text', () => {
+// eslint-disable-next-line no-unused-vars
+import React from 'react'; 
+import MyComponent from '../src/components/ScaleSelector'; 
+
+test('renders select element with populated options', () => {
   render(<MyComponent />);
-  const linkElement = screen.getByText(/hello world/i);
-  expect(linkElement).toBeInTheDocument();
+  const selectElement = screen.getByRole('combobox'); // Assuming the select element is a combobox
+  expect(selectElement).toBeInTheDocument(); // Check if the select element is in the document
 });
