0e47fce0b61d3a8263557fab1050c379a7604b05
Lee Goddard <leegee@gmail.com>
2024-06-26 11:04:02 +0200

    Probability UI

--- src/components/ChordNoteRandomiserComponent.jsx
+++ src/components/ChordNoteRandomiserComponent.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useAtom } from 'jotai';
 
-import styles from './NoteModifier.module.css';
+import styles from './ChordNoteRandomiserComponent.module.css';
 
 import { notesOnAtom } from '../lib/store';
 import { sendNoteWithDuration } from '../lib/midi-messages';
@@ -15,101 +15,106 @@ ChordNoteRandomiserComponent.propTypes = {
     selectedOutput: PropTypes.object.isRequired,
 };
 
-export default function ChordNoteRandomiserComponent({ selectedOutput }) {
-    const [notesOn] = useAtom(notesOnAtom);
-    const [bps, setBps] = useState(INITIAL_BPS);
-    const [alwaysPlayOneNote, setAlwaysPlayOneNote] = useState(true); // State for checkbox
-    const [probabilityThreshold, setProbabilityThreshold] = useState(0.5); // State for probability slider
+export default function ChordNoteRandomiserComponent ( { selectedOutput } ) {
+    const [ notesOn ] = useAtom( notesOnAtom );
+    const [ bps, setBps ] = useState( INITIAL_BPS );
+    const [ playMode, setPlayMode ] = useState( 'oneNote' ); // State for play mode: 'oneNote' or 'probability'
+    const [ probabilityThreshold, setProbabilityThreshold ] = useState( 0.5 ); // State for probability slider
 
-    const handleChangeBps = (event) => {
-        setBps(Number(event.target.value));
+    const handleChangeBps = ( event ) => {
+        setBps( Number( event.target.value ) );
     };
 
-    const handleCheckboxChange = (event) => {
-        setAlwaysPlayOneNote(event.target.checked);
+    const handlePlayModeChange = ( event ) => {
+        setPlayMode( event.target.value );
     };
 
-    const handleProbabilityChange = (event) => {
-        setProbabilityThreshold(Number(event.target.value));
+    const handleProbabilityChange = ( event ) => {
+        setProbabilityThreshold( Number( event.target.value ) );
     };
 
-    function bpsListener() {
-        const pitches = Object.keys(notesOn);
-        if (!pitches.length) {
+    function bpsListener () {
+        const pitches = Object.keys( notesOn );
+        if ( !pitches.length ) {
             return;
         }
 
-        if (alwaysPlayOneNote) {
+        if ( playMode === 'oneNote' ) {
             // Always play one random note
-            const pitch = pitches[Math.floor(Math.random() * pitches.length)];
+            const pitch = pitches[ Math.floor( Math.random() * pitches.length ) ];
             sendNoteWithDuration(
                 pitch,
-                notesOn[pitch].velocity,
+                notesOn[ pitch ].velocity,
                 durationMs,
                 selectedOutput
             );
-        } else {
+        } else if ( playMode === 'probability' ) {
             // Iterate through all notes based on the probability threshold
-            Object.keys(notesOn).forEach((pitch) => {
+            Object.keys( notesOn ).forEach( ( pitch ) => {
                 const probability = Math.random(); // Random number between 0 and 1
-                if (probability < probabilityThreshold) {
+                if ( probability < probabilityThreshold ) {
                     sendNoteWithDuration(
                         pitch,
-                        notesOn[pitch].velocity,
+                        notesOn[ pitch ].velocity,
                         durationMs,
                         selectedOutput
                     );
                 }
-            });
+            } );
         }
     }
 
-    useEffect(() => {
+    useEffect( () => {
         const bpsInterval = 1000 / bps;
-        const bpsTimer = setInterval(bpsListener, bpsInterval);
-        return () => clearInterval(bpsTimer);
-    }, [bps, notesOn, alwaysPlayOneNote, probabilityThreshold]);
+        const bpsTimer = setInterval( bpsListener, bpsInterval );
+        return () => clearInterval( bpsTimer );
+    }, [ bps, notesOn, playMode, probabilityThreshold ] );
 
     return (
         <section className="padded">
             <h2>Chord-note Randomiser</h2>
 
-            <label>
-                Notes per second
+            <div>
                 <input
-                    className={styles.bpsInput}
+                    type="radio"
+                    value="oneNote"
+                    checked={ playMode === 'oneNote' }
+                    onChange={ handlePlayModeChange }
+                />
+                <label>Notes per second</label>
+                <input
+                    className={ styles.bpsInput }
                     type="range"
-                    value={bps}
-                    onChange={handleChangeBps}
+                    value={ bps }
+                    onChange={ handleChangeBps }
                     min="1"
                     max="10"
+                    disabled={playMode !== 'oneNote'}
                 />
-            </label>
+                { bps } notes per second
+            </div>
 
-            <label>
+            <div>
                 <input
-                    type="checkbox"
-                    checked={alwaysPlayOneNote}
-                    onChange={ handleCheckboxChange }
-                    disabled={!alwaysPlayOneNote}
+                    type="radio"
+                    value="probability"
+                    checked={ playMode === 'probability' }
+                    onChange={ handlePlayModeChange }
                 />
-                Always play one note
-            </label>
-
-            <label>
-                Probability threshold
+                <label>Probability threshold</label>
                 <input
-                    className={styles.probabilityInput}
+                    className={ styles.probabilityInput }
                     type="range"
-                    value={probabilityThreshold}
-                    onChange={handleProbabilityChange}
+                    value={ probabilityThreshold }
+                    onChange={ handleProbabilityChange }
                     min="0"
                     max="1"
                     step="0.01"
-                    disabled={alwaysPlayOneNote}
+                    disabled={playMode !== 'probability'}
                 />
-                {probabilityThreshold.toFixed(2)} {/* Display the current value */}
-            </label>
+                { probabilityThreshold.toFixed( 2 ) * 100 }%
+            </div>
+
         </section>
     );
 }
