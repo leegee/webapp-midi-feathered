29866b3853630944c0285ceafdbafa42c1fbc698
Lee Goddard <leegee@gmail.com>
2024-06-26 13:46:07 +0200

    channels

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,10 +1,10 @@
 import { atom } from 'jotai';
+import { MIDI_CHANNEL_IN, MIDI_CHANNEL_OUT } from './constants';
 
 export const midiAccessAtom = atom(null);
 export const midiOutputsAtom = atom({});
 export const selectedOutputAtom = atom( '' );
 export const notesOnAtom = atom({});
-export const scaleRootNoteAtom = atom('C');
-export const scaleNameAtom = atom('major');
-export const scaleNotesAtom = atom([]);
 
+export const midiInputChannelAtom = atom( MIDI_CHANNEL_IN );
+export const midiOutputChannelAtom = atom(MIDI_CHANNEL_OUT);
