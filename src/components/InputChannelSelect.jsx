79553dd7f09987b695cf2f3007d23990f0c121db
Lee Goddard <leegee@gmail.com>
2024-06-26 13:46:36 +0200

    Selectors for channels

---
+++ src/components/InputChannelSelect.jsx
@@ -0,0 +1,23 @@
+import { useAtom } from 'jotai';
+
+import { midiInputChannelAtom } from '../lib/store';
+
+export default function InputChannelSelect () {
+    const [ midiInputChannel, setMidiInputChannel ] = useAtom( midiInputChannelAtom );
+
+    const handleOutputChange = ( event ) => {
+        console.log( 'Set MIDI input channel: ', event.target.value );
+        setMidiInputChannel( Number( event.target.value ) );
+    };
+
+    return (
+        <aside>
+            <label htmlFor="midi-input-channel-selector">MIDI Input Channel: &nbsp;</label>
+            <select id="midi-input-channel-selector" onChange={ handleOutputChange } value={ midiInputChannel }>
+                { Array.from( Array( 16 ).keys() ).map( ( item ) => (
+                    <option key={ item + 1 } value={ item + 1 }>{ item + 1 }</option>
+                ) ) }
+            </select>
+        </aside>
+    );
+}
---
+++ src/components/OutputChannelSelect.jsx
@@ -0,0 +1,23 @@
+import { useAtom } from 'jotai';
+
+import { midiOutputChannelAtom } from '../lib/store';
+
+export default function InputChannelSelect () {
+    const [ midiOutputChannel, setMidiOutputChannel ] = useAtom( midiOutputChannelAtom );
+
+    const handleOutputChange = ( event ) => {
+        console.log( 'Set MIDI output channel: ', event.target.value );
+        setMidiOutputChannel( Number( event.target.value ) );
+    };
+
+    return (
+        <aside>
+            <label htmlFor="midi-input-channel-selector">MIDI Input Channel: &nbsp;</label>
+            <select id="midi-input-channel-selector" onChange={ handleOutputChange } value={ midiOutputChannel }>
+                { Array.from( Array( 16 ).keys() ).map( ( item ) => (
+                    <option key={ item + 1 } value={ item + 1 }>{ item + 1 }</option>
+                ) ) }
+            </select>
+        </aside>
+    );
+}
