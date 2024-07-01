0132cefc5f6065f5a2966fd5e707cf0c98ae3572
Lee Goddard <leegee@gmail.com>
2024-07-01 15:20:43 +0200

    decruft

--- src/lib/midi-messages.js
+++ src/lib/midi-messages.js
@@ -19,7 +19,6 @@ export function sendNoteWithDuration ( pitch, velocity, durationMs, selectedOutp
         stopMidiNote( pitch, selectedOutput, midiChannel );
     }
 
-    console.debug( 'SEND', pitch );
     startMidiNote( pitch, velocity, selectedOutput, midiChannel );
 
     const timer = setTimeout(
