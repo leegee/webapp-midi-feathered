b3eeeaa11c9c888a9bad0c0e6383cacbf8d6259a
Lee Goddard <leegee@gmail.com>
2024-07-01 15:16:49 +0200

    Remove old eslint bits

--- src/components/DeviceSelect.jsx
+++ src/components/DeviceSelect.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useAtom } from 'jotai';
 
 import { midiOutputsAtom, selectedOutputAtom } from '../lib/store';
--- src/components/NotesOnDisplay.jsx
+++ src/components/NotesOnDisplay.jsx
@@ -1,16 +1,14 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
 import { useAtom } from 'jotai';
 
-import {  notesOnAtom } from '../lib/store';
+import { notesOnAtom } from '../lib/store';
 
-export default function NoteList ( ) {
-    const [ notesOn] = useAtom( notesOnAtom );
+export default function NoteList () {
+    const [ notesOn ] = useAtom( notesOnAtom );
     return (
         <ul>
             { Object.entries( notesOn ).map( ( [ key, value ] ) => (
                 <li key={ key }>
-                    { key }: { value.velocity } { value.timestamp } 
+                    { key }: { value.velocity } { value.timestamp }
                 </li>
             ) ) }
         </ul>
