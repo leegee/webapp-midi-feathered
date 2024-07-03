b856783474f2282042cfee2acf912e8ae185ec4f
Lee Goddard <leegee@gmail.com>
2024-07-03 17:28:15 +0200

    Public assets

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import {VitePWA} from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig( {
   base: './',
-  publicDir: 'public',
+  publicDir: 'assets',
 
   build: {
     sourcemap: true,
@@ -16,7 +16,7 @@ export default defineConfig( {
     VitePWA( {
       registerType: 'autoUpdate',
       workbox: {
-        swDest: "public/sw.js",
+        swDest: "assets/sw.js",
         runtimeCaching: [
           {
             urlPattern: /\.(?:png|svg)$/,
