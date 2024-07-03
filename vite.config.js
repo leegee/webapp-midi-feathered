92e22e478c4173d858375ecebd1eadbd1d3fc137
Lee Goddard <leegee@gmail.com>
2024-07-03 17:26:30 +0200

    Public

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import {VitePWA} from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig( {
   base: './',
-  
+  publicDir: 'public',
+
   build: {
     sourcemap: true,
   },
