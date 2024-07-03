dbb8c5694bdb9755ac56c389679ee5a87337053b
Lee Goddard <leegee@gmail.com>
2024-07-03 16:34:30 +0200

    Init PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [ react() ],
+export default defineConfig( {
   base: './',
+  
   build: {
     sourcemap: true,
   },
-})
+  
+  plugins: [
+    react(), 
+    VitePWA( {
+      registerType: 'autoUpdate',
+      workbox: {
+        swDest: "service-worker.js",
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|svg)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+        ],
+      },
+    } ),
+  ],
+} );
