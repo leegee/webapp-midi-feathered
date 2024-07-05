e8b41aafcf054ea1b428fe86fc7937673b536bff
Lee Goddard <leegee@gmail.com>
2024-07-05 12:55:01 +0200

    Auto-gen manifest

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import {VitePWA} from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig( {
@@ -10,11 +10,43 @@ export default defineConfig( {
   build: {
     sourcemap: true,
   },
-  
+
   plugins: [
-    react(), 
+    react(),
     VitePWA( {
       registerType: 'autoUpdate',
+      manifest: {
+        "name": "Feathered MIDI",
+        "short_name": "Feathered MIDI",
+        "start_url": ".",
+        "description": "Featherise MIDI input",
+        "display": "standalone",
+        "theme_color": "#ffffff",
+        "background_color": "#242424",
+        "icons": [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      },
       workbox: {
         swDest: "assets/sw.js",
         runtimeCaching: [
