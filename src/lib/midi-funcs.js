8208b3119ce45adc5bad7e434f5cf09d4dfef21c
Lee Goddard <leegee@gmail.com>
2025-06-19 14:45:41 +0200

    typo

--- src/lib/midi-funcs.js
+++ src/lib/midi-funcs.js
@@ -11,88 +11,93 @@ const timersForPitches = {};
 const DISPATCH_EVENTS_FOR_EXTRAS = true;
 
 // Either a CC value or based on teh note velocity adjusted by  the range
-export const generateVelocity = ( playedNoteVelocity, velocityRange ) => {
+export const generateVelocity = (playedNoteVelocity, velocityRange) => {
     const { maxValue, minValue } = velocityRange;
-    const minPercentageFactor = 1 + ( minValue / 100 );
-    const maxPercentageFactor = 1 + ( maxValue / 100 );
-    const adjustedVelocity = playedNoteVelocity * probabilityTriangular( minPercentageFactor, maxPercentageFactor );
-    return Math.min( Math.max( adjustedVelocity, 0 ), 127 );
+    const minPercentageFactor = 1 + (minValue / 100);
+    const maxPercentageFactor = 1 + (maxValue / 100);
+    const adjustedVelocity = playedNoteVelocity * probabilityTriangular(minPercentageFactor, maxPercentageFactor);
+    return Math.min(Math.max(adjustedVelocity, 0), 127);
 };
 
-export const sendNotes = ( notesOn, rangeState, extensions, midiOutputChannels, selectedOutput ) => {
-    const pitches = Object.keys( notesOn );
-    if ( !pitches.length ) {
+export const sendNotes = (notesOn, rangeState, extensions, midiOutputChannels, selectedOutput) => {
+    const pitches = Object.keys(notesOn);
+    if (!pitches.length) {
         return;
     }
     const usePitches = rangeState.playMode === playModeTypes.MONO
-        ? [ Number( pitches[ Math.floor( Math.random() * pitches.length ) ] ) ]
-        : Object.keys( notesOn ).filter( () => {
-            const probability = probabilityTriangular( 0, 1 );
+        ? [Number(pitches[Math.floor(Math.random() * pitches.length)])]
+        : Object.keys(notesOn).filter(() => {
+            const probability = probabilityTriangular(0, 1);
             return probability < rangeState.polyProbRange.maxValue && probability > rangeState.polyProbRange.minValue;
-        } ).map( usePitch => Number( usePitch ) );
+        }).map(usePitch => Number(usePitch));
 
-    usePitches.forEach( ( aPitch ) => {
-        const useDurationMs = rangeState.speedRange.minValue + Math.random() * ( rangeState.speedRange.maxValue - rangeState.speedRange.minValue );
-        const useVelocity = generateVelocity( notesOn[ [ aPitch ] ], rangeState.velocityRange );
+    usePitches.forEach((aPitch) => {
+        const useDurationMs = rangeState.speedRange.minValue + Math.random() * (rangeState.speedRange.maxValue - rangeState.speedRange.minValue);
+        const useVelocity = generateVelocity(notesOn[[aPitch]], rangeState.velocityRange);
 
-        const useOctave = ( Math.floor(
+        const useOctave = (Math.floor(
             rangeState.octaveRange.minValue == rangeState.octaveRange.maxValue
                 ? rangeState.octaveRange.minValue
-                : probabilityTriangular( rangeState.octaveRange.minValue, rangeState.octaveRange.maxValue )
-        ) - 1 ) * 12;
+                : probabilityTriangular(rangeState.octaveRange.minValue, rangeState.octaveRange.maxValue)
+        ) - 1) * 12;
 
-        const shallUseExtensions = Math.random() > probabilityTriangular( rangeState.extensionsProbRange.minValue, rangeState.extensionsProbRange.maxValue );
-        let useExtenion = 0;
-        if ( shallUseExtensions ) {
-            const activeExtensions = Object.keys( extensions ).filter( ext => extensions[ ext ] );
-            useExtenion = Number( activeExtensions[ Math.floor( Math.random() * activeExtensions.length ) ] );
+        const shallUseExtensions = Math.random() > probabilityTriangular(rangeState.extensionsProbRange.minValue, rangeState.extensionsProbRange.maxValue);
+        let useExtension = 0;
+        if (shallUseExtensions) {
+            const activeExtensions = Object.keys(extensions).filter(ext => extensions[ext]);
+            useExtension = Number(activeExtensions[Math.floor(Math.random() * activeExtensions.length)]);
         }
 
-        let usePitch = aPitch + useOctave + useExtenion;
+        let usePitch = aPitch + useOctave + useExtension;
 
         // Reverse the octave if it puts the note out of range
-        if ( usePitch >= 127 || usePitch < 28 ) {
+        if (usePitch >= 127 || usePitch < 28) {
             usePitch -= useOctave;
-            if ( usePitch >= 127 || usePitch < 28 ) {
-                usePitch -= useExtenion;
+            if (usePitch >= 127 || usePitch < 28) {
+                usePitch -= useExtension;
             }
         }
 
         const midiOutputChannel = midiOutputChannels.length == 1
-            ? midiOutputChannels[ 0 ]                                                        // Use the only output selected
-            : midiOutputChannels[ Math.floor( Math.random() * midiOutputChannels.length ) ]; // Use a random output channel
+            ? midiOutputChannels[0]                                                        // Use the only output selected
+            : midiOutputChannels[Math.floor(Math.random() * midiOutputChannels.length)]; // Use a random output channel
+
+        if (isNaN(usePitch)) {
+            console.warn('-- sendNotes', usePitch)
+        }
 
         try {
-            sendNoteWithDuration( usePitch, useVelocity, useDurationMs, selectedOutput, midiOutputChannel );
+            sendNoteWithDuration(usePitch, useVelocity, useDurationMs, selectedOutput, midiOutputChannel);
         }
-        catch ( e ) {
-            console.error( `usePitch was ${ usePitch }` );
+        catch (e) {
+            console.error(`usePitch was ${usePitch}`);
             throw e;
         }
-    } );
+    });
 }
 
-export function sendNoteWithDuration ( pitch, velocity, durationMs, selectedOutput, midiChannel ) {
+export function sendNoteWithDuration(pitch, velocity, durationMs, selectedOutput, midiChannel) {
     // If the note is playing already, stop it
-    if ( Object.hasOwn( timersForPitches, pitch ) ) {
-        clearTimeout( timersForPitches[ pitch ] );
-        stopMidiNote( pitch, selectedOutput, midiChannel );
+    if (Object.hasOwn(timersForPitches, pitch)) {
+        clearTimeout(timersForPitches[pitch]);
+        stopMidiNote(pitch, selectedOutput, midiChannel);
     }
 
-    startMidiNote( pitch, velocity, selectedOutput, midiChannel );
-    if ( DISPATCH_EVENTS_FOR_EXTRAS ) {
+    startMidiNote(pitch, velocity, selectedOutput, midiChannel);
+
+    if (DISPATCH_EVENTS_FOR_EXTRAS) {
         window.dispatchEvent(
-            new CustomEvent( EVENT_NOTE_START, { detail: { pitch, velocity, midiChannel } } )
+            new CustomEvent(EVENT_NOTE_START, { detail: { pitch, velocity, midiChannel } })
         );
     }
 
     const timer = setTimeout(
         () => {
-            stopMidiNote( pitch, selectedOutput, midiChannel );
-            delete timersForPitches[ pitch ];
-            if ( DISPATCH_EVENTS_FOR_EXTRAS ) {
+            stopMidiNote(pitch, selectedOutput, midiChannel);
+            delete timersForPitches[pitch];
+            if (DISPATCH_EVENTS_FOR_EXTRAS) {
                 window.dispatchEvent(
-                    new CustomEvent( EVENT_NOTE_STOP, { detail: { pitch, velocity, midiChannel } } )
+                    new CustomEvent(EVENT_NOTE_STOP, { detail: { pitch, velocity, midiChannel } })
                 );
             }
 
@@ -100,6 +105,6 @@ export function sendNoteWithDuration ( pitch, velocity, durationMs, selectedOutp
         durationMs
     );
 
-    timersForPitches[ pitch ] = timer;
+    timersForPitches[pitch] = timer;
 }
 
