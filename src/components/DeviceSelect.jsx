bcda3436da2e9b470b6d5f4a5d98c08759a4cad3
Lee Goddard <leegee@gmail.com>
2025-06-19 15:50:39 +0200

    Default to the loopback device if avaialble

--- src/components/DeviceSelect.jsx
+++ src/components/DeviceSelect.jsx
@@ -3,38 +3,38 @@ import { useAtom } from 'jotai';
 
 import { midiOutputsAtom, selectedOutputAtom } from '../lib/store';
 
-function OutputSelect () {
-    const [ midiOutputs ] = useAtom( midiOutputsAtom );
-    const [ selectedOutput, setSelectedOutput ] = useAtom( selectedOutputAtom );
+function OutputSelect() {
+    const [midiOutputs] = useAtom(midiOutputsAtom);
+    const [selectedOutput, setSelectedOutput] = useAtom(selectedOutputAtom);
 
     // Set the selected output when the component mounts
-    useEffect( () => {
-        if ( Object.keys( midiOutputs ).length > 0 ) {
-            const focusriteOutput = Object.entries( midiOutputs ).find( ( [ , output ] ) =>
-                output.name.toLowerCase().includes( 'focusrite' )
+    useEffect(() => {
+        if (Object.keys(midiOutputs).length > 0) {
+            const myFaveOutput = Object.entries(midiOutputs).find(([, output]) =>
+                output.name.toLowerCase().includes('loop')
             );
 
-            if ( focusriteOutput ) {
-                setSelectedOutput( focusriteOutput[ 0 ] ); // [0] is the ID
+            if (myFaveOutput) {
+                setSelectedOutput(myFaveOutput[0]); // [0] is the ID
             } else {
                 // If not found, default to the last output
-                setSelectedOutput( Object.keys( midiOutputs ).slice( -1 )[ 0 ] );
+                setSelectedOutput(Object.keys(midiOutputs).slice(-1)[0]);
             }
         }
-    }, [ midiOutputs, setSelectedOutput ] );
+    }, [midiOutputs, setSelectedOutput]);
 
-    const handleOutputChange = ( event ) => {
-        console.log( 'Set output: ', event.target.value )
-        setSelectedOutput( event.target.value );
+    const handleOutputChange = (event) => {
+        console.log('Set output: ', event.target.value)
+        setSelectedOutput(event.target.value);
     };
 
     return (
         <section>
             <label htmlFor='select-device'>MIDI Device:</label>
-            <select id='select-device' onChange={ handleOutputChange } value={ selectedOutput }>
-                { Object.entries( midiOutputs ).map( ( [ id, output ] ) => (
-                    <option key={ id } value={ id }>{ output.name }</option>
-                ) ) }
+            <select id='select-device' onChange={handleOutputChange} value={selectedOutput}>
+                {Object.entries(midiOutputs).map(([id, output]) => (
+                    <option key={id} value={id}>{output.name}</option>
+                ))}
             </select>
         </section>
     );
