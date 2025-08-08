bf3d12b51bcb8827c916cfcd5a1a432b6e680973
Lee Goddard <leegee@gmail.com>
2025-08-08 12:13:28 +0200

    whitespace

--- src/components/RangeInput.jsx
+++ src/components/RangeInput.jsx
@@ -2,19 +2,19 @@ import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import styles from './RangeInput.module.css';
 
-function debounce ( func, delay ) {
+function debounce(func, delay) {
     let timeoutId;
-    return function ( ...args ) {
-        if ( timeoutId ) {
-            clearTimeout( timeoutId );
+    return function (...args) {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
         }
-        timeoutId = setTimeout( () => {
-            func( ...args );
-        }, delay );
+        timeoutId = setTimeout(() => {
+            func(...args);
+        }, delay);
     };
 }
 
-const RangeInput = ( {
+const RangeInput = ({
     min,
     max,
     minValue,
@@ -25,114 +25,114 @@ const RangeInput = ( {
     size = 'wide',
     flipDisplay = false,
     forceIntegers = false
-} ) => {
-    const [ minPercentage, setMinPercentage ] = useState( ( ( minValue - min ) / ( max - min ) ) * 100 );
-    const [ maxPercentage, setMaxPercentage ] = useState( ( ( maxValue - min ) / ( max - min ) ) * 100 );
+}) => {
+    const [minPercentage, setMinPercentage] = useState(((minValue - min) / (max - min)) * 100);
+    const [maxPercentage, setMaxPercentage] = useState(((maxValue - min) / (max - min)) * 100);
 
-    useEffect( () => {
-        setMinPercentage( ( ( minValue - min ) / ( max - min ) ) * 100 );
-        setMaxPercentage( ( ( maxValue - min ) / ( max - min ) ) * 100 );
-    }, [ minValue, maxValue, min, max ] );
+    useEffect(() => {
+        setMinPercentage(((minValue - min) / (max - min)) * 100);
+        setMaxPercentage(((maxValue - min) / (max - min)) * 100);
+    }, [minValue, maxValue, min, max]);
 
     // Debounce
-    const debounceTimeout = useRef( null );
+    const debounceTimeout = useRef(null);
 
     const debouncedOnChange = useRef(
-        debounce( newValues => {
-            onChange( newValues );
-        }, debounceMs )
+        debounce(newValues => {
+            onChange(newValues);
+        }, debounceMs)
     ).current;
 
-    function handleDebouncedChange ( newValues ) {
-        if ( debounceTimeout.current ) {
-            clearTimeout( debounceTimeout.current );
+    function handleDebouncedChange(newValues) {
+        if (debounceTimeout.current) {
+            clearTimeout(debounceTimeout.current);
         }
-        debounceTimeout.current = setTimeout( () => {
-            if ( forceIntegers ) {
-                newValues.minValue = Math.round( newValues.minValue );
-                newValues.maxValue = Math.round( newValues.maxValue );
+        debounceTimeout.current = setTimeout(() => {
+            if (forceIntegers) {
+                newValues.minValue = Math.round(newValues.minValue);
+                newValues.maxValue = Math.round(newValues.maxValue);
             }
-            debouncedOnChange( newValues );
-        }, debounceMs );
+            debouncedOnChange(newValues);
+        }, debounceMs);
     }
 
-    const handleResize = ( newPercentage, isMin ) => {
-        newPercentage = Math.max( 0, Math.min( 100, newPercentage ) );
-        if ( isMin ) {
-            setMinPercentage( Math.min( newPercentage, maxPercentage - 1 ) );
-            const newValue = min + ( ( max - min ) * Math.min( newPercentage, maxPercentage - 1 ) ) / 100;
-            handleDebouncedChange( { minValue: newValue, maxValue } );
+    const handleResize = (newPercentage, isMin) => {
+        newPercentage = Math.max(0, Math.min(100, newPercentage));
+        if (isMin) {
+            setMinPercentage(Math.min(newPercentage, maxPercentage - 1));
+            const newValue = min + ((max - min) * Math.min(newPercentage, maxPercentage - 1)) / 100;
+            handleDebouncedChange({ minValue: newValue, maxValue });
         } else {
-            setMaxPercentage( Math.max( newPercentage, minPercentage + 1 ) );
-            const newValue = min + ( ( max - min ) * Math.max( newPercentage, minPercentage + 1 ) ) / 100;
-            handleDebouncedChange( { minValue, maxValue: newValue } );
+            setMaxPercentage(Math.max(newPercentage, minPercentage + 1));
+            const newValue = min + ((max - min) * Math.max(newPercentage, minPercentage + 1)) / 100;
+            handleDebouncedChange({ minValue, maxValue: newValue });
         }
     };
 
-    const handleMouseDown = ( e, isMin ) => {
-        e.preventDefault( { passive: false } );
+    const handleMouseDown = (e, isMin) => {
+        e.preventDefault({ passive: false });
         const rect = e.currentTarget.parentElement.getBoundingClientRect();
         const clickPos = vertical ? e.clientY - rect.top : e.clientX - rect.left;
-        const newPercentage = ( clickPos / ( vertical ? rect.height : rect.width ) ) * 100;
-        handleResize( newPercentage, isMin );
+        const newPercentage = (clickPos / (vertical ? rect.height : rect.width)) * 100;
+        handleResize(newPercentage, isMin);
 
         const handleMouseMove = e => {
             const movePos = vertical ? e.clientY - rect.top : e.clientX - rect.left;
-            const newPercentage = ( movePos / ( vertical ? rect.height : rect.width ) ) * 100;
-            handleResize( newPercentage, isMin );
+            const newPercentage = (movePos / (vertical ? rect.height : rect.width)) * 100;
+            handleResize(newPercentage, isMin);
         };
 
         const handleMouseUp = () => {
-            document.removeEventListener( 'mousemove', handleMouseMove );
-            document.removeEventListener( 'mouseup', handleMouseUp );
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
         };
 
-        document.addEventListener( 'mousemove', handleMouseMove );
-        document.addEventListener( 'mouseup', handleMouseUp );
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
     };
 
-    const handleTouchStart = ( e, isMin ) => {
+    const handleTouchStart = (e, isMin) => {
         const rect = e.currentTarget.parentElement.getBoundingClientRect();
-        const touchPos = vertical ? e.touches[ 0 ].clientY - rect.top : e.touches[ 0 ].clientX - rect.left;
-        const newPercentage = ( touchPos / ( vertical ? rect.height : rect.width ) ) * 100;
-        handleResize( newPercentage, isMin );
+        const touchPos = vertical ? e.touches[0].clientY - rect.top : e.touches[0].clientX - rect.left;
+        const newPercentage = (touchPos / (vertical ? rect.height : rect.width)) * 100;
+        handleResize(newPercentage, isMin);
 
         const handleTouchMove = e => {
-            const movePos = vertical ? e.touches[ 0 ].clientY - rect.top : e.touches[ 0 ].clientX - rect.left;
-            const newPercentage = ( movePos / ( vertical ? rect.height : rect.width ) ) * 100;
-            handleResize( newPercentage, isMin );
+            const movePos = vertical ? e.touches[0].clientY - rect.top : e.touches[0].clientX - rect.left;
+            const newPercentage = (movePos / (vertical ? rect.height : rect.width)) * 100;
+            handleResize(newPercentage, isMin);
         };
 
         const handleTouchEnd = () => {
-            document.removeEventListener( 'touchmove', handleTouchMove );
-            document.removeEventListener( 'touchend', handleTouchEnd );
+            document.removeEventListener('touchmove', handleTouchMove);
+            document.removeEventListener('touchend', handleTouchEnd);
         };
 
-        document.addEventListener( 'touchmove', handleTouchMove );
-        document.addEventListener( 'touchend', handleTouchEnd );
+        document.addEventListener('touchmove', handleTouchMove);
+        document.addEventListener('touchend', handleTouchEnd);
     };
 
     return (
-        <section className={ `${ styles[ 'custom-range-input' ] } ${ vertical ? styles.vertical : '' } ${ flipDisplay ? styles.flipped : '' } ${ styles[ size ] }` }>
+        <section className={`${styles['custom-range-input']} ${vertical ? styles.vertical : ''} ${flipDisplay ? styles.flipped : ''} ${styles[size]}`}>
             <div
-                className={ styles.bar }
+                className={styles.bar}
                 style={
                     vertical
-                        ? { top: `${ minPercentage }%`, bottom: `${ 100 - maxPercentage }%` }
-                        : { left: `${ minPercentage }%`, right: `${ 100 - maxPercentage }%` }
+                        ? { top: `${minPercentage}%`, bottom: `${100 - maxPercentage}%` }
+                        : { left: `${minPercentage}%`, right: `${100 - maxPercentage}%` }
                 }
             ></div>
             <div
-                className={ styles.handle }
-                style={ vertical ? { top: `${ minPercentage }%`, width: '100%' } : { left: `${ minPercentage }%` } }
-                onMouseDown={ e => handleMouseDown( e, true ) }
-                onTouchStart={ e => handleTouchStart( e, true ) }
+                className={styles.handle}
+                style={vertical ? { top: `${minPercentage}%`, width: '100%' } : { left: `${minPercentage}%` }}
+                onMouseDown={e => handleMouseDown(e, true)}
+                onTouchStart={e => handleTouchStart(e, true)}
             />
             <div
-                className={ styles.handle }
-                style={ vertical ? { top: `${ maxPercentage }%`, width: '100%' } : { left: `${ maxPercentage }%` } }
-                onMouseDown={ e => handleMouseDown( e, false ) }
-                onTouchStart={ e => handleTouchStart( e, false ) }
+                className={styles.handle}
+                style={vertical ? { top: `${maxPercentage}%`, width: '100%' } : { left: `${maxPercentage}%` }}
+                onMouseDown={e => handleMouseDown(e, false)}
+                onTouchStart={e => handleTouchStart(e, false)}
             />
         </section>
     );
@@ -146,7 +146,7 @@ RangeInput.propTypes = {
     onChange: PropTypes.func.isRequired,
     debounceMs: PropTypes.number,
     vertical: PropTypes.bool,
-    size: PropTypes.oneOf( [ 'normal', 'wide', 'narrow', 'narrowest' ] ),
+    size: PropTypes.oneOf(['normal', 'wide', 'narrow', 'narrowest']),
     forceIntegers: PropTypes.bool,
     flipDisplay: PropTypes.bool,
 };
